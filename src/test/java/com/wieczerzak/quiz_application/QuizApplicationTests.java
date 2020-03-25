c0f35035b99be7daa4aa55190bc8dafcb1426ad6
Jakub Wieczerzak <kuba6328224@gmail.com>
2020-03-25 21:46:21 +0100

    Initialize Spring project

---
+++ src/test/java/com/wieczerzak/quiz_application/QuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wieczerzak.quiz_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
