2ea1d3d942a142cb15baacc5679884ff0bd1c14e
Jakub Wieczerzak <kuba6328224@gmail.com>
2020-03-25 22:09:55 +0100

    Do basic PostMapping and PutMapping

--- src/main/java/com/wieczerzak/quiz_application/QuestionApi.java
+++ src/main/java/com/wieczerzak/quiz_application/QuestionApi.java
@@ -1,9 +1,6 @@
 package com.wieczerzak.quiz_application;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -24,5 +21,15 @@ public class QuestionApi {
         Optional<QuizQuestion> first = quizQuestions.stream().
                 filter(element -> element.getId() == index).findFirst();
         return first.get();
+    }
+
+    @PostMapping //wprowadzanie
+    public boolean addQuestion(@RequestBody QuizQuestion quizQuestion) {
+        return quizQuestions.add(quizQuestion);
+    }
+
+    @PutMapping //nadpisywanie
+    public boolean updateQuestion(@RequestBody QuizQuestion quizQuestion) {
+        return quizQuestions.add(quizQuestion);
     }
 }
