d08141ce96c77fee603dafdcaafa5f7c92aa4406
Jakub Wieczerzak <kuba6328224@gmail.com>
2020-05-11 21:15:38 +0200

    Add logging in services

--- src/main/java/com/wieczerzak/quiz_application/services/QuestionService.java
+++ src/main/java/com/wieczerzak/quiz_application/services/QuestionService.java
@@ -2,12 +2,14 @@ package com.wieczerzak.quiz_application.services;
 
 import com.wieczerzak.quiz_application.dao.entities.QuizQuestion;
 import com.wieczerzak.quiz_application.dao.repositories.QuestionRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
+@Slf4j(topic = "app.logger")
 public class QuestionService {
 
     public QuestionRepository questionRepository;
@@ -20,21 +22,25 @@ public class QuestionService {
 
     public Optional<QuizQuestion> findById(Long id){
         if(questionRepository.existsById(id)){
+            log.debug("Returning data from question repository");
             return questionRepository.findById(id);
         }
         return Optional.empty();
     }
 
     public Iterable<QuizQuestion>findAll(){
+        log.debug("Returning data from question repository");
         return questionRepository.findAll();
     }
 
     public QuizQuestion save(QuizQuestion quizQuestion){
+        log.debug("Saving question in repository");
         return questionRepository.save(quizQuestion);
     }
 
     public void deleteById(Long id){
         if (questionRepository.existsById(id)) {
+            log.debug("Deleting question from repository");
             questionRepository.deleteById(id);
         }
     }
